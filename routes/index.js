de707e99bc2865cda549eb8f0e716c63590768d3
tangtientuan89 <tangtientuan89@gmail.com>
2020-01-15 15:59:45 +0700

    branch master

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',number:'6' });
+});
+
+
+module.exports = router;
+
+router
